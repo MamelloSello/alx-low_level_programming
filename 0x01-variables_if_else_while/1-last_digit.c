e595a62fb8914378f84600b4131db9d957604509
Mamello <mamello88871@gmail.com>
2023-03-19 13:23:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n,n % 10);
 }
 else
 {
-printf("Last digit of %d is %d and is 0\n", n,n % 10);
+printf("Last digit of %d and is less than %d and not 0\n", n,n % 10);
 }
 return (0);
 }
