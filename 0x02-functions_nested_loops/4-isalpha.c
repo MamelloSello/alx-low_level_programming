30a2ebe3dbc91b1cdee121c13cc9064e0f193481
Mamello <mamello88871@gmail.com>
2023-03-22 16:55:50 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
  * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
  *
- * Description: Long description of a function
+ * Description: 'Long description of a function'
  * Don't forgot that a line should not exceed 80 characters.
  * But you're total free to use several lines properly
  * description you function
