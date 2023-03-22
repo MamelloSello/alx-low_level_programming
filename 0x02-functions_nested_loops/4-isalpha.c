a7480f22286b54cdd567c71585026f10c2bdc542
Mamello <mamello88871@gmail.com>
2023-03-22 16:53:28 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,10 @@
  * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
  *
- * Description: Checks for alphabetic characters
+ * Description: Long description of a function
+ * Don't forgot that a line should not exceed 80 characters.
+ * But you're total free to use several lines properly
+ * description you function
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
