27e0e3f1400dcf44a27299aec7e76f4244c010d4
Mamello <mamello88871@gmail.com>
2023-03-22 16:47:06 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
  /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
  *
+ * Description: Checks for alphabetic characters
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
