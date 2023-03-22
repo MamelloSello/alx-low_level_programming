588d0879ce8ceb46a9ff94ee24e6626d1e389687
Mamello <mamello88871@gmail.com>
2023-03-22 16:06:52 -0700

    test

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * return: 1 and prints + if n is greater than zero
+ * 0 and prints o if n is zero
+ * -1 and prints - if n is less than zero
  */
 int _isalpha(int c)
 {
