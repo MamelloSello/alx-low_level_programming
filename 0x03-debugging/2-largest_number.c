066d6acc9044a3a0d02097788654222624115cfc
Mamello <mamello88871@gmail.com>
2023-03-25 14:51:47 -0700

    0x03. C - Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,9 +11,13 @@ int largest_number(int a, int b, int c)
 int largest;
 if (a > b && a > c)
 {
-largest = c;
+largest = a;
 }
 else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
 {
 largest = b;
 }
