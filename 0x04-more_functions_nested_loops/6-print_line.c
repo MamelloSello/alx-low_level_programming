196ac76f097680d6ca14c661290534a077ffc747
Mamello <mamello88871@gmail.com>
2023-03-25 13:37:05 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,8 +2,9 @@
 /**
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
+ * Return: terminal draw
  */
-void print_line(int n )
+void print_line(int n)
 {
 if (n <= 0)
 {
