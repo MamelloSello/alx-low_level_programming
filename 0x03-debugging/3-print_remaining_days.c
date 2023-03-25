6fc921ff9a5e783ddfa1d590c98127a9138ce551
Mamello <mamello88871@gmail.com>
2023-03-25 14:57:26 -0700

    0x03. C - Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,11 +8,11 @@
 * @year: year
 * Return: void
 */
-void print_remaining_days(int month,int day, int year) 
+void print_remaining_days(int month, int day, int year)
 {
-if((year % 100 == 0 && year % 400  == 0 || (year % 4 == 0)))
+if ((year % 100 == 0 && year % 400  == 0 || (year % 4 == 0)))
 {
-if(month > 2 && day >= 60)
+if (month > 2 && day >= 60)
 {
 day++;
 }
@@ -21,9 +21,9 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if(month == 2 && day == 60)
+if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02/%04d\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
