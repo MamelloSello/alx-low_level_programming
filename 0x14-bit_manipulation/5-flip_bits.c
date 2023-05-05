b5588142c4d71b3f81cbd77e4b172655d1dbdd4f
Mamello <mamello88871@gmail.com>
2023-05-05 14:38:37 -0700

    updated

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* flip_bits -  Returns the number of bits that needed to flip to get from one number to another
+* 
+* @n: First number
+* @m: Second number
+*
+* Return: Number of bits to change
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int j, 
+num = 0;
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
+for (j = 63; j >= 0; j--)
+{
+current = exclusive >> j;
+if (current & 1)
+num++;
+}
+return (num);
+}
